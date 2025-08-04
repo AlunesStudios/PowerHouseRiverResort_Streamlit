9fefe83bd0d7253063ef27e17a4b9e3ea6971677
AlunesStudios <macablastbytk@gmail.com>
2025-08-04 21:29:31 +0530

    Update Navigation.py

--- PRR_Streamlit_python/Navigation.py
+++ PRR_Streamlit_python/Navigation.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from PIL import Image
-st.set_page_config(page_title="PowerHouse River Resort", layout="centered")
-st.logo(Main_Image)
+from PIL import Image 
 pages = {"About us": [st.Page("Main.py", title="Main")],"Register": [st.Page("App.py", title="Book Now!")]}
 X = st.navigation(pages)
 X.run()
