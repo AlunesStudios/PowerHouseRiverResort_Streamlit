b570aca531af3ab281c7fc78f782f3ca6b071e1a
AlunesStudios <macablastbytk@gmail.com>
2025-08-04 21:54:41 +0530

    Delete PRR_Streamlit_python directory

--- PRR_Streamlit_python/Navigation.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from PIL import Image 
-pages = {"About us": [st.Page("Main.py", title="Main")],"Register": [st.Page("App.py", title="Book Now!")]}
-X = st.navigation(pages)
-X.run()
--- PRR_Streamlit_python/test.py
+++
@@ -1,3 +0,0 @@
-import streamlit as st
-for i in range(10000000000):
-    st.balloons()
