a9ae2121460addd7851bfaa2231675f62aa78706
AlunesStudios <senulatk@gmail.com>
2025-09-26 11:24:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,6 @@ import streamlit as st
 from PIL import Image
 from datetime import date, timedelta
 
-from test import password
 
 Main_Image = Image.open("PRR_Main_image.png")
 st.set_page_config(page_title="PowerHouse River Resort")
